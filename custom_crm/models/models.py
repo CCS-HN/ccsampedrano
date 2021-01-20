4379170ede84d66225b24dfeaebb3e1d4916f433
Fabrica Hamilton <sistemas@fabricahamilton.com>
2021-01-19 23:53:26 -0600

    Update Formulario CRM

--- custom_crm/models/models.py
+++ custom_crm/models/models.py
@@ -81,19 +81,19 @@ class CRM(models.Model):
     # _sql_constraints = [('name_unique', 'unique(name)', 'Este Alumno ya esta registrado!')]
 
 
-    # @api.model
-    # def create(self, values):
-    #     # Override the original create function for the crm.lead model
-    #     record = super(CRM, self).create(values)
+    @api.model
+    def create(self, values):
+        # Override the original create function for the crm.lead model
+        record = super(CRM, self).create(values)
  
-    #     # Change the values of a variable in this super function
-    #     record['id_alumno_int'] = int(self.id_alumno)
-    #     # print 'Passed this function. passed_override_write_function value: ' + str(record['passed_override_write_function'])
+        # Change the values of a variable in this super function
+        record['id_alumno_int'] = int(record.id_alumno)
+        # print 'Passed this function. passed_override_write_function value: ' + str(record['passed_override_write_function'])
  
-    #     # Return the record so that the changes are applied and everything is stored.
-    #     return record
+        # Return the record so that the changes are applied and everything is stored.
+        return record
 
-    # _sql_constraints = [('id_alumno_int_unique', 'unique(id_alumno_int)', 'Este alumno ya esta registrado!')]
+    _sql_constraints = [('id_alumno_int_unique', 'unique(id_alumno_int)', 'Este alumno ya esta registrado!')]
 
 class CRM_modalidad(models.Model):
     _name = 'crm.lead.modalidad'
